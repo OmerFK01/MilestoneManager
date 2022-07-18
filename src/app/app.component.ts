84ed326a17ba7f3c10e64991eb2a96f60cce9b1c
omer.khan <omer.khan@fdmgroup.com>
2022-07-18 13:17:25 -0400

    Update-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Employee } from './employee';
-import { EmployeeService } from './employee.service';
+import { NgForm } from '@angular/forms';
+import { Consultant } from './consultant';
+import { ConsultantService } from './consultant.service';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +10,114 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
-  public employees: Employee[] = [];
+export class AppComponent implements OnInit {
+  public consultants!: Consultant[];
+  public editConsultant: Consultant | undefined;
+  public deleteConsultant: Consultant | undefined;
 
-  constructor(private employeeService: EmployeeService){}
+  constructor(private consultantService: ConsultantService) { }
 
   ngOnInit(): void {
-    this.getEmployees();
+    this.getConsultants();
   }
 
-  public getEmployees(): void{
-    this.employeeService.getEmployees().subscribe(
-      (response: Employee[]) => {
-        this.employees = response;
+  public getConsultants(): void {
+    this.consultantService.getConsultants().subscribe(
+      (response: Consultant[]) => {
+        this.consultants = response;
       },
-      (error: HttpErrorResponse) =>{
+      (error: HttpErrorResponse) => {
         alert(error.message);
       }
     );
   }
-  //  title = 'milestoneManagerApp';
+
+  public onAddConsultant(addForm: NgForm): void {
+    document.getElementById('add-consultant-form')?.click();
+    this.consultantService.addConsultant(addForm.value).subscribe(
+      (response: Consultant) => {
+        console.log(response);
+        this.getConsultants();
+        addForm.reset();
+      }
+      ,
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    );
+  }
+
+  public onUpdateConsultant(consultant: Consultant): void {
+    this.consultantService.updateConsultant(consultant).subscribe(
+      (response: Consultant) => {
+        console.log(response);
+        this.getConsultants();
+      }
+      ,
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    );
+  }
+
+  public onDeleteConsultant(consultantId: number): void {
+    this.consultantService.deleteConsultant(consultantId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getConsultants();
+      }
+      ,
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    );
+  }
+
+  public searchConsultant(key: String){
+    console.log(key);
+    const results: Consultant[] =[];
+    if(this.consultants !== null){
+      for (const consultant of this.consultants){
+        if(consultant.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || consultant.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || consultant.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || consultant.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1)
+        {
+          results.push(consultant);
+        }
+      }
+      this.consultants = results;
+      if(results.length === 0 || !key){
+        this.getConsultants();
+      }
+      }
+
+
+  }
+
+
+  public onOpenModal(mode: String, consultant?: Consultant): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addConsultantModal');
+    }
+    else if (mode === 'edit') {
+      this.editConsultant = consultant;
+      button.setAttribute('data-target', '#updateConsultantModal');
+    }
+    else if (mode === 'delete') {
+      this.deleteConsultant = consultant;
+      button.setAttribute('data-target', '#deleteConsultantModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
 }
